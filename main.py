c3b921528db9823f08c811ec870873cd91a84914
Alysson <alyssos@hotmail.com>
2024-05-22 14:36:48 -0300

    feat:teste commit

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('ffgf')
+    print_hi('Alysson')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
